d67a44b7fb042af3420908c78f0337f7c054e9ef
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-23 16:06:55 +0530

    add setup createproduct feature and CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+	public record CreateProductCommand
+	(
+		string Name,
+		string Description,
+		string ImageFile,
+		decimal Price,
+		List<string> Category
+	) : IRequest<CreateProductResult>;
+
+	public record CreateProductResult(Guid Id);
+
+	internal class CreateProductCommandHandler
+	{
+	}
+}
