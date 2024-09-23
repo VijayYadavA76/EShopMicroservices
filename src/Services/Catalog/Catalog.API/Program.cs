e1d76c6df6c935479f29337ac995daf5d1ea5b05
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-23 18:02:02 +0530

    test working of catalog create-product api

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,11 +3,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
 
 app.Run();
