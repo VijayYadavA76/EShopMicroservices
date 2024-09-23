0487be2b8416e1e831a80ad18b136d24daf79675
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-23 18:42:08 +0530

    add marten lib for postgres db

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
+﻿
 namespace Catalog.API.Products.CreateProduct
 {
 	public record CreateProductCommand
@@ -14,7 +12,8 @@ namespace Catalog.API.Products.CreateProduct
 
 	public record CreateProductResult(Guid Id);
 
-	internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+	internal class CreateProductCommandHandler(IDocumentSession session)
+		: ICommandHandler<CreateProductCommand, CreateProductResult>
 	{
 		public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
 		{
@@ -29,8 +28,11 @@ namespace Catalog.API.Products.CreateProduct
 				Category = command.Category
 			};
 			// save to database
+			session.Store( product );
+			await session.SaveChangesAsync(cancellationToken);
+
 			// return CreateProductResult
-			return new CreateProductResult(Guid.NewGuid());
+			return new CreateProductResult(product.Id);
 		}
 	}
 }
