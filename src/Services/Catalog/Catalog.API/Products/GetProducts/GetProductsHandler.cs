d167e466e2f1c64ef1e53b0fa8f4d35e41027a0b
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-24 16:38:18 +0530

    add exception and create GetProductById Api

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -3,7 +3,7 @@ namespace Catalog.API.Products.GetProducts
 {
 	public record GetProductsQuery() : IQuery<GetProductsResult>;
 	public record GetProductsResult(IEnumerable<Product> Products);
-	public class GetProductsQueryHandler 
+	internal class GetProductsQueryHandler 
 		(IDocumentSession session,ILogger<GetProductsQueryHandler> logger)
 		: IQueryHandler<GetProductsQuery, GetProductsResult>
 	{
