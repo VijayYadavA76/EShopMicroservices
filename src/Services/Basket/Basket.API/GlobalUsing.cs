682f988d176c9b5f377d842a666427ec32bb8620
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-26 12:35:44 +0530

    add basket repository

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,7 +1,9 @@
 ﻿global using Carter;
+global using Marten;
 global using Mapster;
 global using MediatR;
 global using FluentValidation;
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using Basket.API.Exceptions;
 global using BuildingBlocks.Behaviors;
