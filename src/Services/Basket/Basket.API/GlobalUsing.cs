281c0907f9fcc7ec1ce5a3595d217d666e50a91f
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-26 12:12:44 +0530

    add get, delete,store basket endpoints and handler

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,1 +1,7 @@
-﻿
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using FluentValidation;
+global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
