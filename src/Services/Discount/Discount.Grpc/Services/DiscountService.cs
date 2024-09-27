b157bc2abea25aeb4795b3c1a1874509a94b104a
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-27 12:12:48 +0530

    create a discount proto file and its service

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+	public class DiscountService : Discount.DiscountBase
+	{
+		private readonly ILogger<DiscountService> _logger;
+		public DiscountService(ILogger<DiscountService> logger)
+		{
+			_logger = logger;
+		}
+
+		public override Task<DiscountReply> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+		{
+			return Task.FromResult(new DiscountReply
+			{
+				ProductName = "Hello " + request.ProductName
+			});
+		}
+	}
+}
