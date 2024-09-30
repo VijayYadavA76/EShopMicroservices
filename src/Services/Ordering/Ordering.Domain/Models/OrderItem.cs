9904ee2107327bfdcb40124c8ded737db2add361
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-09-30 16:48:54 +0530

    added models and global using file

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Models
+{
+	public class OrderItem : Entity<Guid>
+	{
+		internal OrderItem(Guid orderId, Guid productId, decimal quantity, int price ) 
+		{ 
+			OrderId = orderId;
+			ProductId = productId;
+			Price = price;
+			Quantity = quantity;
+		}
+		public Guid OrderId { get; private set; } = default!;
+		public Guid ProductId { get; private set; } = default!;
+		public int Price { get; private set; } = default!;
+		public decimal Quantity { get; private set; } = default!;
+	}
+}
