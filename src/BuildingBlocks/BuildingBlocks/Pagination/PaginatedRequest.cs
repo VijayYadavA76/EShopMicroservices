63fc37534737289f92122b9acea74e27b1a4b224
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-10-04 15:09:51 +0530

    added getorders with pagination

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace BuildingBlocks.Pagination
+{
+	public record PaginatedRequest(int pageIndex = 0, int pageSize = 10);
+
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,33 @@
+﻿
+using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+	public class GetOrdersHandler(IApplicationDbContext dbContext)
+		: IQueryHandler<GetOrdersQuery, GetOrdersResult>
+	{
+		public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+		{
+			var pageIndex = query.PaginatedRequest.pageIndex;
+			var pageSize = query.PaginatedRequest.pageSize;
+			var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+			var orders = 
+				await dbContext.Orders
+					.Include(o => o.OrderItems)
+					.AsNoTracking()
+					.Skip(pageIndex * pageSize)
+					.Take(pageSize)
+					.OrderBy(o => o.OrderName)
+					.ToListAsync();
+			
+			return new GetOrdersResult(
+				new PaginatedResult<OrderDto>
+					(
+						pageIndex: pageIndex,
+						pageSize: pageSize,
+						count: totalCount ,
+						data: orders.ToOrderDtoList()
+					));
+		}
+	}
+}
