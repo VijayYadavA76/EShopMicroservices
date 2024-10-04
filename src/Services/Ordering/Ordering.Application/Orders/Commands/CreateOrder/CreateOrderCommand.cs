f2cbabb5f71b4bc0f5ed3f1b22bdac43b2c5f7dd
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-10-04 10:59:07 +0530

    created updateorder and its handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderCommand(OrderDto Order)
+        : ICommand<CreateOrderResult>;
+    public record CreateOrderResult(Guid Id);
+
+    public class CreateOrderCommandValidater : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidater()
+        {
+            RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("OrderName is required");
+            RuleFor(o => o.Order.CustomerId).NotNull().WithMessage("CustomerId is not null");
+            RuleFor(o => o.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder
+{
+	public record UpdateOrderCommand(OrderDto Order) 
+		: ICommand<UpdateOrderResult>;
+	public record UpdateOrderResult(bool IsSuccess);
+
+	public class UpdateOrderValidator : AbstractValidator<UpdateOrderCommand>
+	{
+		public UpdateOrderValidator()
+		{
+			RuleFor(o => o.Order.OrderName).NotEmpty().WithMessage("OrderName is required");
+			RuleFor(o => o.Order.CustomerId).NotNull().WithMessage("CustomerId is not null");
+			RuleFor(o => o.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+		}
+	}
+}
