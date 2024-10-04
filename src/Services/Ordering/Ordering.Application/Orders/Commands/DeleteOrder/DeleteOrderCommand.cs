cdefbd7123c6a048c50a7f9d8a917440cd9d3065
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-10-04 11:34:22 +0530

    created deleteorder and its handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+	public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+	public record DeleteOrderResult(bool IsSSuccess);
+
+	public class DeleteOrderValidator : AbstractValidator<DeleteOrderCommand>
+	{
+		public DeleteOrderValidator()
+		{
+			RuleFor(o => o.OrderId).NotNull().WithMessage("OrderId is required");
+		}
+	}
+}
