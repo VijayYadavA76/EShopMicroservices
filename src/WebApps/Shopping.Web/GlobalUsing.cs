112521559a0e716b2fdcb025230c198cbb0d577b
Vijay Yadav <vijay.yadav@intelegencia.com>
2024-10-16 17:15:57 +0530

    added index.cshtml.cs

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,4 +1,6 @@
 ﻿global using Refit;
 global using Shopping.Web.Services;
+global using Microsoft.AspNetCore.Mvc;
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Catalog;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
